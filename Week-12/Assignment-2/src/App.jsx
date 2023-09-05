69572abfbf4b938ea7877a69e9644a4123fe4fd6
priyayashraj <51752490+priyayashraj@users.noreply.github.com>
2023-09-05 12:45:29 +0530

    Assignment-2 react js

---
+++ Week-12/Assignment-2/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import BookListWithLogging from './components/BooklistWithLogging';
+
+function App() {
+  return (
+    <BookListWithLogging/>
+  );
+}
+
+export default App;
